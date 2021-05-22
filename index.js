a74326add901b96bffdc7912089052a52144c7ba
Jelena <jelena.durovic@powerhrg.com>
2021-05-21 20:50:19 -0400

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation){
+ const hq = 42
+ if (hq > pickUpLocation){
+    return hq - pickUpLocation
+ } else {
+    return pickUpLocation - hq
+ }
+}
+
+function distanceFromHqInFeet(distance){
+    return distanceFromHqInBlocks(distance) * 264
+
+}
+
+function distanceTravelledInFeet(start, end){
+  if (end > start){
+    return (end - start) * 264
+  } else {
+    return (start - end) * 264
+  }
+
+}
+
+function calculatesFarePrice(start, end){
+
+  if (distanceTravelledInFeet(start, end) <= 400){
+    return 0
+  } else if (distanceTravelledInFeet(start, end) >400 && distanceTravelledInFeet(start, end) <= 2000){
+    return (distanceTravelledInFeet(start, end) - 400) * 0.02
+  } else if (distanceTravelledInFeet(start, end) > 2000 && distanceTravelledInFeet(start, end) <= 2500) {
+    return 25
+  } else if (distanceTravelledInFeet(start, end) > 2500) {
+    return "cannot travel that far"
+  }
+  }
+  
+
+  
+
+  
+  
+
